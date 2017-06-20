b6a966d281a37d018bc0e21518bb0a51b9f27194
Ilya Kuydich <agree2012@yandex.ru>
2017-06-20 17:31:41 +0400

    Add files via upload

---
+++ test/MyAppTests.ts
@@ -0,0 +1,23 @@
+import { MyApp } from "../src/MyApp";
+
+describe("areStringsEqual", () => {
+    it("should return true if two strings are the same", () => {
+        var result =
+            new MyApp
+            .MyStringUtils("whatever", "whoever")
+            .areStringsEqual("t", "t");
+
+        expect(result).toEqual(true);
+    });
+});
+
+describe("areStringsEqual", () => {
+    it("should return true if two strings are the same", () => {
+        var result =
+            new MyApp
+            .MyStringUtils("whatever", "whoever")
+            .areStringsEqual("t", "t");
+
+        expect(result).toEqual(true);
+    });
+});
